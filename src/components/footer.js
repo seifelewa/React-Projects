0b0aa712e0aa07d697cde8f399a2d467e588fcc9
Mansi74 <yousefmansi74@gmail.com>
2021-09-14 13:48:47 +0200

    footer

---
+++ src/components/footer.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class footer extends React.Component {
+    render() { 
+        return (
+            <div class="footer">
+                <p className="title">
+                    Develpoment &amp; IT
+                </p>
+                <p className="title">
+                    Creative &amp; Design
+                </p>
+                <p className="title">
+                    Sales &amp; Marketing
+                </p>
+                <p className="title">
+                    Writing &amp; Translation
+                </p>
+            </div>
+        );
+    }
+}
+ 
+export default footer;
