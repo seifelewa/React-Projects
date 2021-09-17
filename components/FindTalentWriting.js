24105d46d14ab3f7c2d080813bb2c0c6900f486d
maryamnouhh <82498068+maryamnouhh@users.noreply.github.com>
2021-09-17 19:33:18 +0200

    Add files via upload

---
+++ components/FindTalentWriting.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class FindTalentWriting extends React.Component {
+    render() { 
+        return (
+            <div className='find-talent-writing'>
+                <h1> Find Talent </h1>
+                <ul>
+                    <Link to="/find-talent-it"> <li className='major'>Development &amp; IT</li> </Link>
+                    <Link to="/find-talent-creative"> <li className='major'>Creative &amp; Design </li> </Link>
+                    <Link to="/find-talent-sales"> <li className='major'>Sales &amp; Marketing </li> </Link>
+                    <Link to="/find-talent-writing"> <li className='major'>Writing &amp; Translation</li> </Link>
+
+                    <button className='minor-1'> <li> Business Writing </li> </button>
+                    <button className='minor-2'> <li> Language Localization </li> </button>
+                    <button className='minor-3'> <li> Creative Writing </li> </button>
+                    <button className='minor-4'> <li> Editing &amp; Proofreading </li> </button>
+
+                </ul>
+            </div>
+        );
+    }
+}
+ 
+export default FindTalentWriting;
