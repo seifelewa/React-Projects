40a3dfebab24d8f2446cf97831c30e31b6f19698
MeGaXV <71066760+MeGaXV@users.noreply.github.com>
2021-09-19 18:52:55 +0200

    Edits by seif and megahed

--- src/contexts/employeeContext.js
+++ src/contexts/employeeContext.js
@@ -1,41 +1,15 @@
-import React, {createContext, useState} from "react";
+import React, {createContext, useState, useEffect} from "react";
 
 
 export const EmpContext = createContext();
 
 const EmpContextProvider = (props) =>{
-    const [emps, setEmps] = useState([
-        {name: "Abdel-Rahman", rate: 20, rating: 1, jobCount: 32},
-        {name: "Seif Elewa", rate: 30, rating: 3.5, jobCount:24},
-        {name: "Maryam Nouh", rate: 40, rating: 5, jobCount:57},
-        {name: "Youssef Mansi", rate: 50, rating: 4, jobCount:68},
-        {name: "Youssef Mansi", rate: 50, rating: 4, jobCount:68},
-        {name: "Youssef Mansi", rate: 50, rating: 4, jobCount:68},
-        {name: "Youssef Mansi", rate: 50, rating: 4, jobCount:68},
-        {name: "Youssef Mansi", rate: 50, rating: 4, jobCount:68},
-        {name: "Youssef Mansi", rate: 50, rating: 4, jobCount:68},
-        {name: "Youssef Mansi", rate: 50, rating: 4, jobCount:68},
-        {name: "Youssef Mansi", rate: 50, rating: 4, jobCount:68},
-        {name: "Youssef Mansi", rate: 50, rating: 4, jobCount:68},
-        {name: "Youssef Mansi", rate: 50, rating: 4, jobCount:68},
-        {name: "Youssef Mansi", rate: 50, rating: 4, jobCount:68},
-        {name: "Youssef Mansi", rate: 50, rating: 4, jobCount:68},
-        {name: "Youssef Mansi", rate: 50, rating: 4, jobCount:68},
-        {name: "Youssef Mansi", rate: 50, rating: 4, jobCount:68},
-        {name: "Youssef Mansi", rate: 50, rating: 4, jobCount:68},
-]);
-    // useEffect(() =>{
-    //     fetchData().then(res => setEmps(res.data))
-    // },[])
-    // const fetchData = async () => {
-    //     try {
-    //         const response = await fetch();
-    //         const json = await response.json();
-    //         console.log(json);
-    //         } catch (error) {
-    //         console.log("error", error);
-    //         }
-    // };
+    const [emps, setEmps] = useState('');
+    useEffect(() =>{
+        fetch("/test")
+        .then((res) => res.json())
+        .then((data) => setEmps(data));
+    }, []);
     return(
         <EmpContext.Provider value={{emps}}>
             {props.children}
