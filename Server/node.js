2b9932b7b162ca272c0c0d07dd5df637fc15ed45
seifelewa <44884545+seifelewa@users.noreply.github.com>
2021-09-21 04:07:21 +0200

    Update node.js

--- Server/node.js
+++ Server/node.js
@@ -1,38 +1,38 @@
 const express = require("express");
-const path = require('path');
-const PORT = 3000;
+const path = require("path");
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, '../React-project/build')))
+app.use(express.static(path.resolve(__dirname, "../React-project/build")));
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
-app.get('/test', (req,res)=>{
-    res.json([ {name: "Abdel-Rahman", rate: 20, rating: 1, jobCount: 32},
-    {name: "Seif Elewa", rate: 30, rating: 3.5, jobCount:24},
-    {name: "Maryam Nouh", rate: 40, rating: 5, jobCount:57},
-    {name: "Youssef Mansi", rate: 50, rating: 4, jobCount:68},
-    {name: "Youssef Mansi", rate: 50, rating: 4, jobCount:68},
-    {name: "Youssef Mansi", rate: 50, rating: 4, jobCount:68},
-    {name: "Youssef Mansi", rate: 50, rating: 4, jobCount:68},
-    {name: "Youssef Mansi", rate: 50, rating: 4, jobCount:68},
-    {name: "Youssef Mansi", rate: 50, rating: 4, jobCount:68},
-    {name: "Youssef Mansi", rate: 50, rating: 4, jobCount:68},
-    {name: "Youssef Mansi", rate: 50, rating: 4, jobCount:68},
-    {name: "Youssef Mansi", rate: 50, rating: 4, jobCount:68},
-    {name: "Youssef Mansi", rate: 50, rating: 4, jobCount:68},
-    {name: "Youssef Mansi", rate: 50, rating: 4, jobCount:68},
-    {name: "Youssef Mansi", rate: 50, rating: 4, jobCount:68},
-    {name: "Youssef Mansi", rate: 50, rating: 4, jobCount:68},
-    {name: "Youssef Mansi", rate: 50, rating: 4, jobCount:68},
-    {name: "Youssef Mansi", rate: 50, rating: 4, jobCount:68}
+app.get("/test", (req, res) => {
+  res.json([
+    { name: "Abdel-Rahman", rate: 20, rating: 1, jobCount: 32 },
+    { name: "Seif Elewa", rate: 30, rating: 3.5, jobCount: 24 },
+    { name: "Maryam Nouh", rate: 40, rating: 5, jobCount: 57 },
+    { name: "Youssef Mansi", rate: 50, rating: 4, jobCount: 68 },
+    { name: "Youssef Mansi", rate: 50, rating: 4, jobCount: 68 },
+    { name: "Youssef Mansi", rate: 50, rating: 4, jobCount: 68 },
+    { name: "Youssef Mansi", rate: 50, rating: 4, jobCount: 68 },
+    { name: "Youssef Mansi", rate: 50, rating: 4, jobCount: 68 },
+    { name: "Youssef Mansi", rate: 50, rating: 4, jobCount: 68 },
+    { name: "Youssef Mansi", rate: 50, rating: 4, jobCount: 68 },
+    { name: "Youssef Mansi", rate: 50, rating: 4, jobCount: 68 },
+    { name: "Youssef Mansi", rate: 50, rating: 4, jobCount: 68 },
+    { name: "Youssef Mansi", rate: 50, rating: 4, jobCount: 68 },
+    { name: "Youssef Mansi", rate: 50, rating: 4, jobCount: 68 },
+    { name: "Youssef Mansi", rate: 50, rating: 4, jobCount: 68 },
+    { name: "Youssef Mansi", rate: 50, rating: 4, jobCount: 68 },
+    { name: "Youssef Mansi", rate: 50, rating: 4, jobCount: 68 },
+    { name: "Youssef Mansi", rate: 50, rating: 4, jobCount: 68 },
   ]);
-    
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../React-project/build', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../React-project/build", "index.html"));
 });
