8fd822fea7e32d98e22dc638c00df6434d7b7b4f
maryamnouhh <82498068+maryamnouhh@users.noreply.github.com>
2021-09-21 12:30:26 +0200

    Update NavBar.js

--- React-project/src/components/NavBar.js
+++ React-project/src/components/NavBar.js
@@ -7,12 +7,12 @@ class NavBar extends React.Component {
         return (
             <div className="nav-bar">
                 <ul>
-                    <Link to="/"> <li className="nav-bar-1">Aptitude<span className='s1'>Share</span> </li> </Link>
-                    <Link to="/find-talent-it"> <li className="nav-bar-1"> Talent </li> </Link>
-                    <Link to="/find-work-it"> <li className="nav-bar-1"> Work </li> </Link>
+                    <Link className='link' to="/"> <li className="nav-bar-1">Aptitude<span className='s1'>Share</span> </li> </Link>
+                    <Link className='link' to="/find-talent-it"> <li className="nav-bar-1"> Talent </li> </Link>
+                    <Link className='link' to="/find-work-it"> <li className="nav-bar-1"> Work </li> </Link>
 
-                    <Link to="/login"> <button className="button-1"> <li> Log In </li></button> </Link>
-                    <Link to="/signup"> <button className="button-2"> <li> Sign Up </li></button> </Link>
+                    <Link className='link' to="/login"> <button className="button-1"> <li> Log In </li></button> </Link>
+                    <Link className='link' to="/signup"> <button className="button-2"> <li> Sign Up </li></button> </Link>
                 </ul>
             </div>
         );
