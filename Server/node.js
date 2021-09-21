253fab2417794d07d876f7a5c51ecf1ce6cfd976
MeGaXV <71066760+MeGaXV@users.noreply.github.com>
2021-09-21 17:58:37 +0200

    DB stuff

--- Server/node.js
+++ Server/node.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+
+const dbURL="mongodb+srv://Seifelewa:s12e12@cluster0.dwyp7.mongodb.net/FullStack-Project?retryWrites=true&w=majority";
+mongoose.connect(dbURL).then((result)=>{
+  app.listen(process.env.PORT||3000);
+  console.log("Connected successfully")}).catch((err)=>console.log("Connection failed"));
+
 app.use(express.static(path.resolve(__dirname, "../React-project/build")));
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
 
 app.get("/test1", (req, res) => {
   res.json({
