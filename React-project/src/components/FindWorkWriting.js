cdb28ca9df699b24e143d45736ef6ee9f9f54de3
maryamnouhh <82498068+maryamnouhh@users.noreply.github.com>
2021-09-21 12:31:58 +0200

    Update FindWorkWriting.js

--- React-project/src/components/FindWorkWriting.js
+++ React-project/src/components/FindWorkWriting.js
@@ -7,10 +7,10 @@ class FindWorkWriting extends React.Component {
             <div className='find-work-writing'>
                 <h1> Find Work </h1>
                 <ul>
-                    <Link to="/find-work-it"> <li className='major'>Development &amp; IT</li> </Link>
-                    <Link to="/find-work-creative"> <li className='major'>Creative &amp; Design </li> </Link>
-                    <Link to="/find-work-sales"> <li className='major'>Sales &amp; Marketing </li> </Link>
-                    <Link to="/find-work-writing"> <li className='major'>Writing &amp; Translation</li> </Link>
+                    <Link className='link' to="/find-work-it"> <li className='major'>Development &amp; IT</li> </Link>
+                    <Link className='link' to="/find-work-creative"> <li className='major'>Creative &amp; Design </li> </Link>
+                    <Link className='link' to="/find-work-sales"> <li className='major'>Sales &amp; Marketing </li> </Link>
+                    <Link className='link' to="/find-work-writing"> <li className='major'>Writing &amp; Translation</li> </Link>
 
                     <button className='minor-1'> <li> Business Writing </li> </button>
                     <button className='minor-2'> <li> Language Localization </li> </button>
