7de5a4bfe24b2aaddd8d8f733662b9548ebb3437
seifelewa <44884545+seifelewa@users.noreply.github.com>
2021-09-22 16:39:56 +0200

    footer

--- React-project/src/components/Footer.js
+++ React-project/src/components/Footer.js
@@ -4,11 +4,13 @@ class Footer extends React.Component {
   render() {
     return (
       <div class="footer">
-        <p className="title">Develpoment &amp; IT</p>
-        <p className="title">Creative &amp; Design</p>
-        <p className="title">Sales &amp; Marketing</p>
+        <p className="title">Develpoment &amp; IT </p>
+        <p className="title">Creative &amp; Design </p>
+        <p className="title">Sales &amp; Marketing </p>
         <p className="title">Writing &amp; Translation</p>
-        <p>Copyright &copy; Seif,Youssef,Maryam,Abdelrahman 2021 </p>
+        <p className="copy">
+          Copyright &copy; Seif,Youssef,Maryam,Abdelrahman 2021{" "}
+        </p>
       </div>
     );
   }
