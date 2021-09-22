5555455501550193c32f94c9c9d2b66728615e56
seifelewa <44884545+seifelewa@users.noreply.github.com>
2021-09-22 17:26:26 +0200

    what

--- React-project/src/components/jobTable.js
+++ React-project/src/components/jobTable.js
@@ -1,53 +1,51 @@
-import React, {useContext} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import { JobContext } from '../contexts/jobContext';
+import React, { useContext } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import { JobContext } from "../contexts/jobContext";
 
-
-
-const JobTable = () =>{
-    const {jobs} = useContext(JobContext);
-    const classes = makeStyles({
-        table: {
-          width: 650,
-          overflowY: "scroll",
-        },
-      });
-    return (
-        <div >
-            <TableContainer component={Paper}>
-            <Table className={classes.table} id="table2" aria-label="simple table">
-                <TableHead>
-                <TableRow>
-                    <TableCell>Job Name</TableCell>
-                    <TableCell align="right">Pay Rate</TableCell>
-                    <TableCell align="right">Level</TableCell>
-                    <TableCell align="right">Apply Now</TableCell>
-                </TableRow>
-                </TableHead>
-                <TableBody>
-                {jobs.map((job) => (
-                    <TableRow key={job.id}>
-                    <TableCell component="th" scope="emp">
-                        {job.type}
-                    </TableCell>
-                    <TableCell align="right">{job.pay_Rate} $/hr</TableCell>
-                    <TableCell align="right">{job.level}</TableCell>
-                    <TableCell align="right">
-                                <button  className="jobApplyBt">Apply</button>
-                        </TableCell>
-                    </TableRow>
-                ))}
-                </TableBody>
-            </Table>
-            </TableContainer>
+const JobTable = () => {
+  const { jobs } = useContext(JobContext);
+  const classes = makeStyles({
+    table: {
+      width: 650,
+      overflowY: "scroll",
+    },
+  });
+  return (
+    <div className="wow">
+      <TableContainer component={Paper}>
+        <Table className={classes.table} id="table2" aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Job Name</TableCell>
+              <TableCell align="right">Pay Rate</TableCell>
+              <TableCell align="right">Level</TableCell>
+              <TableCell align="right">Apply Now</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {jobs.map((job) => (
+              <TableRow key={job.id}>
+                <TableCell component="th" scope="emp">
+                  {job.type}
+                </TableCell>
+                <TableCell align="right">{job.pay_Rate} $/hr</TableCell>
+                <TableCell align="right">{job.level}</TableCell>
+                <TableCell align="right">
+                  <text className="jobApplyBt">Apply</text>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
-}
+};
 export default JobTable;
--- Server/node.js
+++ Server/node.js
@@ -1,64 +1,75 @@
 const express = require("express");
 const path = require("path");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
-const Employer = require("./models/Employer")
+const Employer = require("./models/Employer");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const dbURL="mongodb+srv://Seifelewa:s12e12@cluster0.dwyp7.mongodb.net/FullStack-Project?retryWrites=true&w=majority";
-mongoose.connect(dbURL).then((result)=>{
-  app.listen(process.env.PORT||3001);
-  console.log("Connected successfully")}).catch((err)=>console.log("Connection failed"));
+const dbURL =
+  "mongodb+srv://Seifelewa:s12e12@cluster0.dwyp7.mongodb.net/FullStack-Project?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURL)
+  .then((result) => {
+    app.listen(process.env.PORT || 3001);
+    console.log("Connected successfully");
+  })
+  .catch((err) => console.log("Connection failed"));
 
 app.use(express.static(path.resolve(__dirname, "../React-project/build")));
 
-
 app.get("/test1", (req, res) => {
   res.json({
-    message: [{name: "Abdel-Rahman", rate:20, rating:1, jobCount:30},
-    {name: "Maryam Nouh", rate:40, rating:3, jobCount:56},
-    {name: "Seif Elewa", rate:30, rating:2, jobCount:43},
-    {name: "Youssef Mansi", rate:20, rating:5, jobCount:99},
-  ]
+    message: [
+      { name: "Abdel-Rahman", rate: 20, rating: 1, jobCount: 30 },
+      { name: "Maryam Nouh", rate: 40, rating: 3, jobCount: 56 },
+      { name: "Seif Elewa", rate: 30, rating: 2, jobCount: 43 },
+      { name: "Youssef Mansi", rate: 20, rating: 5, jobCount: 99 },
+    ],
   });
 });
 
 app.get("/test2", (req, res) => {
   res.json({
-    message: [ {id:1 ,type: "Full Stack Development", pay_Rate:"20", level:"Intermideiate"},
-    {id:2 ,type: "Sales", pay_Rate:"30", level:"Expert"},
-    {id:3 ,type: "UX Design", pay_Rate:"10", level:"Beginner"},
-    {id:4 ,type: "AI Development", pay_Rate:"20", level:"Intermideiate"},
-    {id:5 ,type: "Java Development", pay_Rate:"35", level:"Intermideiate"}
-  ]
+    message: [
+      {
+        id: 1,
+        type: "Full Stack Development",
+        pay_Rate: "20",
+        level: "Intermediate",
+      },
+      { id: 2, type: "Sales", pay_Rate: "30", level: "Expert" },
+      { id: 3, type: "UX Design", pay_Rate: "10", level: "Beginner" },
+      { id: 4, type: "AI Development", pay_Rate: "20", level: "Intermediate" },
+      {
+        id: 5,
+        type: "Java Development",
+        pay_Rate: "35",
+        level: "Intermediate",
+      },
+    ],
   });
 });
 
+app.post("/loginTest", (req, res) => {
+  username = req.body.username;
+  password = req.body.password;
 
-app.post("/loginTest", (req,res)=>{
-    username=req.body.username;
-    password= req.body.password;
-    
-    Employer.findOne({username}, (err, res2)=>{
-      if(err) throw err;
-      const user= res2;
-      
-      if(password === user.Password){
-        console.log("success");
-        res.redirect('/find-talent-it');
-      } else{
-        console.log("failure");
-        res.redirect('/login');
-      }
-    }).catch((err)=> console.log(err));
+  Employer.findOne({ username }, (err, res2) => {
+    if (err) throw err;
+    const user = res2;
 
+    if (password === user.Password) {
+      console.log("success");
+      res.redirect("/find-talent-it");
+    } else {
+      console.log("failure");
+      res.redirect("/login");
+    }
+  }).catch((err) => console.log(err));
 });
 
-
 app.get("*", (req, res) => {
-  res.sendFile(
-    path.resolve(__dirname, "../React-project/build", "index.html")
-  );
+  res.sendFile(path.resolve(__dirname, "../React-project/build", "index.html"));
 });
