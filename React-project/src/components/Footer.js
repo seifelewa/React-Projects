3e7ba4415269e87a798e696b746ca226077df994
seifelewa <44884545+seifelewa@users.noreply.github.com>
2021-09-22 15:29:25 +0200

    Update Footer.js

--- React-project/src/components/Footer.js
+++ React-project/src/components/Footer.js
@@ -1,24 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Footer extends React.Component {
-    render() { 
-        return (
-            <div class="footer">
-                <p className="title">
-                    Develpoment &amp; IT
-                </p>
-                <p className="title">
-                    Creative &amp; Design
-                </p>
-                <p className="title">
-                    Sales &amp; Marketing
-                </p>
-                <p className="title">
-                    Writing &amp; Translation
-                </p>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div class="footer">
+        <p className="title">Develpoment &amp; IT</p>
+        <p className="title">Creative &amp; Design</p>
+        <p className="title">Sales &amp; Marketing</p>
+        <p className="title">Writing &amp; Translation</p>
+        <p>Copyright &copy; Seif,Youssef,Maryam,Abdelrahman 2021 </p>
+      </div>
+    );
+  }
 }
- 
+
 export default Footer;
